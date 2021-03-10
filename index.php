501a3d23283c160444fdd13e5fcc30d20e506e16
benz830 <77878804+benz830@users.noreply.github.com>
2021-03-10 20:36:48 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,11 +8,30 @@
 <b><h1><font color="black"><center><mark> THE STORY OF POVERTY </b></h1></font></center></mark>
 <br><b><h2><font color="purple"> Tomorrow </b></h2><font>
 <font color="yellow"><b>Message for hope:
+ <br> Aldrin Chua
 <br>In our situation today this pandemic, many people loses hope on having a good health.  Many people experiences stress because people find 
 <br>it hard to find money but we should never lose hope to find ways because there are many ways to solve the problem.  To those who are grately 
 <br>affected by the coronavirus pandemic I hope that this trials will not let us lose our hope and weaken us.  Let us make this pandemic strengthen
- <br>our faith and hope to our God. And also never forget to seek help to our almighty God. So lets us be determined to be strong enough to face this
+<br>our faith and hope to our God. And also never forget to seek help to our almighty God. So lets us be determined to be strong enough to face this
 <br> pandemic. Let us in trust everything to God. Without our God there will no light in this darkest moments so let us keep our faith strong.
+ <br>
+ <br> Jasmine Villanueva
+<br> Last year was a very rough one. We've been to many struggles and hardships. I'm pleased and amazed by those people who are struggling but still fighting
+<br> because I know that it affects our day and mood when we have a problem and cannot think suitably, but that didn't stop them. We have to keep going because 
+<br> life gave us this so that we can be strong and have the courage to keep moving. They were greatly affected by the pandemic but they didn't choose to give 
+<br> up which symbolizes dove. It flies high, like if nothing is going on in their surroundings. They are focusing only on their destination.
+ <br>
+ <br> Benz Martinez
+ <br>For all those who are greatly affected by the pandemic, we hope that you will rise and fight this pandemic with the presence of God. We hope that you will 
+ <br>not surrender while fighting this pandemic and stay safe in your home, and we hope that God will glorify you and give you many blessings after you were greatly 
+ <br>affected by this pandemic. We make a design that is the dove that symbolizes peace to make our world peaceful after this pandemic makes the world chaotic. 
+ <br>Because this pandemic ruins many people's lives that turn the world into chaos. And also many people were greatly affected because of this today’s pandemic. 
+ <br>That's why we decided to draw a dove that represents peace to make people who got affected by this pandemic have peace and to prevent this world from chaos.
+ <br>
+ <br> Rogil Estalila
+ <br>Since I can't really do much to those people who are affected deeply by this pandemic I just want to let you know that I'm praying for your guidance and protection.
+<br> And I know we can pass this problem together. And don't lose hope because there are a lot of people who are kind enough to help. And the government are doing their
+<br> best to overcome this pandemic that we are facing.
 <p align="center"><img width ="150px" img src="peace.jpg">
 <b><h2><font color="orange">Video about Poverty </B></h2></font>
 <video src="vd.mp4" video width="260" height="160" controls></video>
